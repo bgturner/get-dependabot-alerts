1ed1c0bca3aed31003b4afb6953f0662ada2520d
Benjamin Turner <benjamin@passionsplay.com>
2022-05-11 09:38:02 -0700

    feat: process each arg
    Takes args in the form of `org/repo` and iterates over them by
    splitting at the slash.

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 
 import dotenv from "dotenv";
 dotenv.config();
+
+const repos = process.argv.slice(2);
+
+repos.forEach((item) => {
+  const [org, repo] = item.split("/");
+  console.log("org: ", org);
+  console.log("repo: ", repo);
+});
